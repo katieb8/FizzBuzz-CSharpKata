722613ee4650d6755bee3c5815df2e919ba6a1ac
Katie Burdett <katie.burdett@ao.com>
2023-12-13 15:38:18 +0000

    chore: initial commit with int and fizz buzz test cases

---
+++ Tests/FizzBuzzTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class FizzBuzzGenerateTest
+    {
+        private Generator _generator;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            _generator = new Generator();
+        }
+
+        [TestCase(2, "2"), 
+         TestCase(4, "4"),
+         TestCase(7, "7")
+        ]
+        public void CanReturnInputNumber2(int userInput, string expectedResult)
+        {
+            var result = _generator.Generate(userInput);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(3, "Fizz")]
+        public void CanReturnFizzOnInputDivisibleBy3(int userInput, string expectedResult)
+        {
+            var result = _generator.Generate(userInput);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Tests/Generator.cs
@@ -0,0 +1,14 @@
+﻿namespace Tests
+{
+    public class Generator
+    {
+        public string Generate(int userInput)
+        {
+            if (userInput % 3 == 0)
+            {
+                return ("Fizz");
+            }
+            return userInput.ToString();
+        }
+    }
+}
